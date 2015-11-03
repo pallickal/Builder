pallickal/Builder

commit 9e41fed05dc9a80a08f7a5f4ad335beb2e59c76b

Use getters to retrieve token cookies
-less brittle if underlying store changes
-reduces $cookies dependency bloat

app/session/session.js
@@ -1,5 +1,5 @@
 angular.module('session', ['token', 'tenantTokens'])
-  .factory('sessionFactory', function($http, $cookies, $cacheFactory, $q, $window, tokenService, tenantTokensService) {
+  .factory('sessionFactory', function($http, $cacheFactory, $q, $window, tokenService, tenantTokensService) {
     return {
       authenticate: authenticate,
       withToken: withToken,
@@ -12,12 +12,7 @@ angular.module('session', ['token', 'tenantTokens'])
 
     function withToken() {
       var deferred = $q.defer();
-      var token = $cookies.getObject('X-Subject-Token');
-
-      console.log(
-        "sessionFactory:withToken - |X-Subject-Token| = " +
-        JSON.stringify($cookies.getObject('X-Subject-Token'), null, '  ')
-      );
+      var token = tokenService.get();
 
       if (token) {
         var min_till_exp = moment(token.expires_at).diff(moment(), 'minutes');
@@ -70,12 +65,7 @@ angular.module('session', ['token', 'tenantTokens'])
 
     function withTenantToken(tenant_id) {
       var deferred = $q.defer();
-      var token = $cookies.getObject(tenant_id);
-
-      console.log(
-        'sessionFactory:withTenantToken - |' + tenant_id + '| = ' +
-        JSON.stringify($cookies.getObject(tenant_id), null, '  ')
-      );
+      var token = tenantTokensService.get();
 
       function renew() {
         withToken()

app/session/tenantTokens.js
@@ -1,9 +1,18 @@
 angular.module('tenantTokens', ['token', 'session'])
   .service('tenantTokensService', function($interval, $http, $cookies, tokenService) {
     return {
+      get: get,
       setDirty: setDirty,
       renew: renew
     };
+
+    function get(tenant_id) {
+      console.log(
+        'tenantTokensService:get - |' + tenant_id + '| = ' +
+        JSON.stringify($cookies.getObject(tenant_id), null, '  ')
+      );
+      return $cookies.getObject(tenant_id);
+    }
 
     function setDirty(tenant_id) {
       console.log('tenantTokensService:setDirty - setting tenant ' + tenant_id + ' token dirty');

app/session/token.js
@@ -2,6 +2,7 @@ angular.module('token', [])
   .service('tokenService', function($interval, $q, $http, $cookies) {
     return {
       init: init,
+      get: get,
       setDirty: setDirty,
       renew: renew
     };
@@ -41,6 +42,14 @@ angular.module('token', [])
           deferred.reject('tokenService:get - HTTP failure response:' + JSON.stringify(err_response, null, '  '))
         });
       return deferred.promise;
+    }
+
+    function get() {
+      console.log(
+        "tokenService:get - |X-Subject-Token| = " +
+        JSON.stringify($cookies.getObject('X-Subject-Token'), null, '  ')
+      );
+      return $cookies.getObject('X-Subject-Token');
     }
 
     function setDirty() {

commit 66c099d2c7b36c81cce64377fdd69b0d086623fa

Use get/set within token services
- also fix a bug introduced in commit 9e41fed05d where the tenant
  token is passed in as undefined to get(tenant_id), forcing
  it to get renewed every single time

app/session/session.js
@@ -65,7 +65,7 @@ angular.module('session', ['token', 'tenantTokens'])
 
     function withTenantToken(tenant_id) {
       var deferred = $q.defer();
-      var token = tenantTokensService.get();
+      var token = tenantTokensService.get(tenant_id);
 
       function renew() {
         withToken()

app/session/tenantTokens.js
@@ -16,9 +16,9 @@ angular.module('tenantTokens', ['token'])
 
     function setDirty(tenant_id) {
       console.log('tenantTokensService:setDirty - setting tenant ' + tenant_id + ' token dirty');
-      var token = $cookies.getObject(tenant_id);
+      var token = get(tenant_id);
       token.dirty = true;
-      $cookies.putObject(tenant_id, token, {expires: token.expires_at});
+      set(tenant_id, token);
     };
 
     function renew(subject_token_id, tenant_id, deferred) {
@@ -54,11 +54,10 @@ angular.module('tenantTokens', ['token'])
         'expires_at': data.access.token.expires,
         'stored_at': moment().toISOString()
       };
+      set(tenant_id, token);
+    }
 
-      $cookies.putObject(tenant_id, token, {expires: data.access.token.expires});
-      console.log(
-        'tenantTokensService:persist - |' + tenant_id + '| = ' +
-        JSON.stringify($cookies.getObject(tenant_id), null, '  ')
-      );
+    function set(tenant_id, token) {
+      $cookies.putObject(tenant_id, token, {expires: token.expires_at});
     }
   });

app/session/token.js
@@ -54,13 +54,13 @@ angular.module('token', [])
 
     function setDirty() {
       console.log('tokenService:setDirty - setting token dirty');
-      var token = $cookies.getObject('X-Subject-Token');
+      var token = get();
       token.dirty = true;
-      $cookies.putObject('X-Subject-Token', token, {expires: token.expires_at});
+      set(token);
     };
 
     function renew(deferred) {
-      var token = $cookies.getObject('X-Subject-Token');
+      var token = get();
 
       var requestData = {
         "auth": {
@@ -85,6 +85,10 @@ angular.module('token', [])
       );
     }
 
+    function set(token) {
+      $cookies.putObject('X-Subject-Token', token, {expires: token.expires_at});
+    }
+
     function persist(x_subject_token, expires_at) {
       var token = {
         'id': x_subject_token,
@@ -92,10 +96,6 @@ angular.module('token', [])
         'expires_at': expires_at,
         'stored_at': moment().toISOString()
       };
-
-      $cookies.putObject('X-Subject-Token', token, {expires: expires_at});
-      console.log('tokenService:persist - |X-Subject-Token| = ' +
-        JSON.stringify($cookies.getObject('X-Subject-Token'), null, '  ')
-      );
+      set(token);
     }
   });