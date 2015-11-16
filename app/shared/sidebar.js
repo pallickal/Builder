2773168f6708cf93e9eab2eabfcc53ea02d6521a
Lindsay Pallickal <pallickal@gmail.com>
2015-11-16 18:46:28 -0500

    Bootstrap sidebar and use stateParams for links
    Watch for $stateChangeSuccess and use it to update which tab is
    highlighted, if in fact the state has changed.
    
    Use the tab.stateParams in the html instead of re-creating it there
    with individual variable(s), in line with DRY principles.
    
    Update applicable tab.stateParams to have the current tenantId when
    currentTenantId is updated. This ensures that the tenantId dependent
    links still update dynamically when currentTenantId is updated.
    
    Use ng-href instead of ui-sref, because $scope variables inside {{ }}
    blocks are not dynamically updating with ui-sref. See links below.
    
    http://stackoverflow.com/questions/24349731/dynamically-set-the-value-of-ui-sref-angularjs
    https://github.com/angular-ui/ui-router/issues/395

--- app/shared/sidebar.js
+++ app/shared/sidebar.js
@@ -1,8 +1,42 @@
 angular.module('sidebar', ['tenants'])
   .controller('sidebarCtrl', function($scope, $state, Tenants) {
+    $scope.currentTenantId = Tenants.currentTenantId();
+    $scope.tabs = [
+      {
+        title:'Projects',
+        state:'app.tenants'
+      },
+      {
+        title:'Servers',
+        state:'app.servers',
+        stateParams: { tenantId: $scope.currentTenantId }
+      }
+    ];
+    var tenantIdParamStates = ['app.servers'];
+    $scope.$state = $state;
+
+    function syncActiveTabToState() {
+      for (var i = 0; i < $scope.tabs.length; i++) {
+        $scope.tabs[i].active = (
+          $state.includes($scope.tabs[i].state) ? true : false
+        );
+      }
+    }
+
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams,
+      fromState, fromParams) {
+      if (fromState.name != toState.name) syncActiveTabToState();
+    });
+
+    $scope.$watch('currentTenantId', function(currentTenantId) {
+      for (var i = 0; i < $scope.tabs.length; i++) {
+        if (tenantIdParamStates.indexOf($scope.tabs[i].state) >= 0) {
+          $scope.tabs[i].stateParams.tenantId = currentTenantId;
+        }
+      }
+    });
+
     $scope.$on('tenants:currentTenant:updated', function (event, tenantId) {
       $scope.currentTenantId = tenantId;
     });
-
-    $scope.currentTenantId = Tenants.currentTenantId();
   });
