f3c20ae60e609112bdcfef52da21e4ee0478f28e
Lindsay Pallickal <pallickal@gmail.com>
2015-11-17 07:13:46 -0500

    Return servers array from Servers.list()
    list() previously returned the response data from its http request, with
    just one entry in its top level in the hash, 'servers'. With
    $scope.servers assigned this value, iterating all the servers from html
    required referencing servers.servers, which is redundant. list() now
    picks out the lone servers array item from the object and returns that.

--- app/servers/Servers.js
+++ app/servers/Servers.js
@@ -7,7 +7,7 @@ angular.module('servers', ['tenants'])
     function list(tenantId) {
       return $http.get('http://192.168.122.183:8774/v2.1/' + tenantId + '/servers')
         .then(function(response) {
-          return response.data;
+          return response.data.servers;
         }, function(response) {
           return $q.reject(new Error('Could not get server list for tenant ' + tenantId));
         });
